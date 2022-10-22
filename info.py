f149a6bb79ed1b6fadfeb931a6c170da7c5a87f4
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-22 16:20:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,6 +39,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
+IMDL = int(environ.get('FILTER_DELETE_TIME', "900"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
