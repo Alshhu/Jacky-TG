f39ceacee85c0c14b8dc7410c37835231c388605
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-23 10:22:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
-WELCOME_PICS = environ.get("WELCOME_PICS", "https://telegra.ph/file/85105adc2622868877cfa.jpg")
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PICS = (environ.get('PICS', 'https://telegra.ph/file/2e2a07e86066538ed7406.jpg')).split()
+WELCOME_PICS = (environ.get("WELCOME_PICS", "https://graph.org/file/8a57f8dc5cf440dbced08.jpg")).split()
 
 
 # Admins, Channels & Users
@@ -35,19 +35,19 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 IMDL = int(environ.get('FILTER_DELETE_TIME', "900"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'http_movieshubhere')
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🎪 <b>ᴛɪᴛʟᴇ</b> :   <b><a href={url}>{title}</a></b>\n\n🔖 <b>ɪɴꜰᴏ</b> :   <b>{runtime}ᴍɪɴ | {release_date}</b>\n\n🌟 <b>ʀᴀᴛɪɴɢ</b> :   <b><a href={url}/ratings>{rating}</a> /10</b>\n🎭 <b>ɢᴇɴʀᴇꜱ</b> :   <b>{genres}</b>\n🎙 <b>ʟᴀɴɢ</b> :   <b><code>{languages}</code></b>\n\n❣ <b>ᴩᴏᴡᴇʀᴇᴅ ʙy</b> :   <b><a href=https://t.me/cinemala_com1>ᴄɪɴᴇᴍᴀʟᴀ.ᴄᴏᴍ</a></b>")
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "{file_caption}")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🎪 <b>ᴛɪᴛʟᴇ</b> :   <b><a href={url}>{title}</a></b>\n\n🔖 <b>ɪɴꜰᴏ</b> :   <b>{runtime}ᴍɪɴ | {release_date}</b>\n\n🌟 <b>ʀᴀᴛɪɴɢ</b> :   <b><a href={url}/ratings>{rating}</a> /10</b>\n🎭 <b>ɢᴇɴʀᴇꜱ</b> :   <b>{genres}</b>\n🎙 <b>ʟᴀɴɢ</b> :   <b><code>{languages}</code></b>\n\n❣ <b>ᴩᴏᴡᴇʀᴇᴅ ʙy</b> :   <b><a href=https://t.me/http_movieshubhere>ᴄɪɴᴇᴍᴀʟᴀ.ᴄᴏᴍ</a></b>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
